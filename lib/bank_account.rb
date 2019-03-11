3aebc92078cab46a133a3cd062763e2044216555
thomasmarano <thomas.marano2@gmail.com>
2019-03-11 15:01:23 +0000

    Passing test for adding money to your account balance

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,13 @@
 class BankAccount
+
     attr_reader :balance
+
     def initialize
         @balance = 0
+    end
+
+    def deposit(amount)
+        @balance += amount
     end
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -3,29 +3,25 @@ require 'bank_account'
 
 describe BankAccount do
 
-  it 'initializes with a balance of 0' do
-    expect(subject.balance).to eq(0)
+  before(:each) do
+      @bankaccount = BankAccount.new
   end
 
+
+  it 'initializes with a balance of 0' do
+    expect(@bankaccount.balance).to eq(0)
+  end
+
+  describe '#deposit' do
+    it 'responds to deposit' do
+      expect(@bankaccount).to respond_to(:deposit).with(1).argument
+    end
+    it 'lets you deposit money in your account' do
+      expect(@bankaccount.deposit(10)).to eq(10)
+    end
+  end
+
+  
+
+
 end
-
-
-#
-# require 'oystercard'
-# require 'journey'
-#
-# describe Oystercard do
-#
-#   let(:station){ double :station }
-#
-#   before(:each) do
-#     @card = Oystercard.new
-#     @card.topup(20)
-#     @amount = double("amount")
-#     @entry_station = double("entry station")
-#     @exit_station = double("exit station")
-#   end
-#
-#   it "has a balance" do
-#     expect(subject).to respond_to(:balance)
-#   end
