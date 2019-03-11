d90f07e2541588ac00903d1b9b31476e982a1a99
thomasmarano <thomas.marano2@gmail.com>
2019-03-11 14:46:48 +0000

    First Commit

---
+++ spec/bank_account_spec.rb
@@ -0,0 +1,32 @@
+require 'bankaccount'
+
+
+# describe Bankaccount do
+#
+#   it 'has a balance' do
+#     expect(subject).to respond_to(:balance)
+#   end
+#
+#
+# end
+
+
+#
+# require 'oystercard'
+# require 'journey'
+#
+# describe Oystercard do
+#
+#   let(:station){ double :station }
+#
+#   before(:each) do
+#     @card = Oystercard.new
+#     @card.topup(20)
+#     @amount = double("amount")
+#     @entry_station = double("entry station")
+#     @exit_station = double("exit station")
+#   end
+#
+#   it "has a balance" do
+#     expect(subject).to respond_to(:balance)
+#   end
